1735d7ae3b92e6b3d6fb646362d4f2836af189f0
Küppers <Peer>
2020-11-26 14:06:58 +0100

    changed sql-file encoding to utf-8

--- DSCB140 - Fallstudie Webshop - DDL.sql
+++ DSCB140 - Fallstudie Webshop - DDL.sql
@@ -13,7 +13,8 @@ create table kategorie (
 katid int primary key,
 bezeichnung varchar(50)
 );
-
+
+
 create table benutzer (
 email varchar(100) primary key,
 passwort varchar(50) not null,
@@ -31,47 +32,49 @@ verfuegbarkeit int not null,
 preis decimal(6,2) not null,
 katid int not null,
 foreign key (katid) references kategorie(katid) on delete cascade
-);
+);
+
 
 -- -----------------
 
 create table abteilung (
 abtid int primary key,
 bezeichnung varchar(50),
-uebergeordnet_abtid int, -- Fremdschlüssel hier nicht NOT NULL! Kardinalitäten der Hierarchie!
-foreign key (uebergeordnet_abtid) references abteilung (abtid) on delete cascade  -- Fremdschlüssel-Constraint
-);
+uebergeordnet_abtid int, -- FremdschlÃ¼ssel hier nicht NOT NULL! KardinalitÃ¤ten der Hierarchie!
+foreign key (uebergeordnet_abtid) references abteilung (abtid) on delete cascade  -- FremdschlÃ¼ssel-Constraint
+);
+
 -- -----------------
 
 create table kunde (
-email varchar(100) primary key, -- Primärschlüssel und gleichzeitg Fremdschlüssel
+email varchar(100) primary key, -- PrimÃ¤rschlÃ¼ssel und gleichzeitg FremdschlÃ¼ssel
 kdnr int unique,  -- Spalte soll eindeutig sein
 strasse varchar(50),
 hausnummer varchar(10),
 plz varchar(5),
 ort varchar(50),
 geburtsdatum date,
-foreign key (email) references benutzer (email) on delete cascade  -- Fremdschlüssel-Constraint
+foreign key (email) references benutzer (email) on delete cascade  -- FremdschlÃ¼ssel-Constraint
 );
 
 create table mitarbeiter (
-email varchar(100) primary key, -- Primärschlüssel und gleichzeitg Fremdschlüssel
+email varchar(100) primary key, -- PrimÃ¤rschlÃ¼ssel und gleichzeitg FremdschlÃ¼ssel
 manr int unique,  -- Spalte soll eindeutig sein
 telefonnr varchar(30),
 abtid int not null,
-foreign key (email) references benutzer (email) on delete cascade,  -- Fremdschlüssel-Constraint email
-foreign key (abtid) references abteilung (abtid) on delete cascade  -- Fremdschlüssel-Constraint abtid
+foreign key (email) references benutzer (email) on delete cascade,  -- FremdschlÃ¼ssel-Constraint email
+foreign key (abtid) references abteilung (abtid) on delete cascade  -- FremdschlÃ¼ssel-Constraint abtid
 );
 -- ----------------------------
 
 create table bestellung ( 
-bestnr int auto_increment primary key, -- automatische Hochzählen aktivieren
+bestnr int auto_increment primary key, -- automatische HochzÃ¤hlen aktivieren
 bestelldatum TIMESTAMP,
-bearbeitet_email varchar(100) not null,  -- min-Kardinalität 1
+bearbeitet_email varchar(100) not null,  -- min-KardinalitÃ¤t 1
 foreign key (bearbeitet_email) references mitarbeiter(email) on delete cascade, -- FK-Constraint
-kunde varchar(100) not null,  -- min-Kardinalität 1
+kunde varchar(100) not null,  -- min-KardinalitÃ¤t 1
 foreign key (kunde) references kunde(email) on delete cascade, -- FK-Constraint
-statusid int not null, -- min-Kardinalität 1
+statusid int not null, -- min-KardinalitÃ¤t 1
 foreign key (statusid) references status(statusid) on delete cascade -- FK-Constraint
 );
 
@@ -80,28 +83,28 @@ select * from bestellung;
 -- --------------------
 
 create table warenkorbposition (
-kunde_email varchar(100), -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (kunde_email) references kunde(email) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
-pnr int, -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (pnr) references produkt(pnr) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
+kunde_email varchar(100), -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (kunde_email) references kunde(email) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
+pnr int, -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (pnr) references produkt(pnr) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
 menge int,
-primary key (kunde_email, pnr) -- Definition eines zusammengesetzten Primärschlüssels
+primary key (kunde_email, pnr) -- Definition eines zusammengesetzten PrimÃ¤rschlÃ¼ssels
 );
 
 
 create table bestellposition (	
-bestnr int, -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (bestnr) references bestellung(bestnr) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
-pnr int, -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (pnr) references produkt(pnr) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
+bestnr int, -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (bestnr) references bestellung(bestnr) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
+pnr int, -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (pnr) references produkt(pnr) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
 menge int,
-primary key (bestnr, pnr) -- Definition eines zusammengesetzten Primärschlüssels, 8);
+primary key (bestnr, pnr) -- Definition eines zusammengesetzten PrimÃ¤rschlÃ¼ssels, 8);
 );
 
 create table gemeinsamerkauf (
-k1_katid int, -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (k1_katid) references kategorie(katid) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
-k2_katid int, -- wird Teil des Primärschlüssels (s.u.), daher NOT NULL nicht nötig
-foreign key (k2_katid) references kategorie(katid) on delete cascade, -- gleichzeitig Teil vom Primärschlüssel und Fremdschlüssel
-primary key (k1_katid, k2_katid) -- Definition eines zusammengesetzten Primärschlüssel1, 2);
+k1_katid int, -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (k1_katid) references kategorie(katid) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
+k2_katid int, -- wird Teil des PrimÃ¤rschlÃ¼ssels (s.u.), daher NOT NULL nicht nÃ¶tig
+foreign key (k2_katid) references kategorie(katid) on delete cascade, -- gleichzeitig Teil vom PrimÃ¤rschlÃ¼ssel und FremdschlÃ¼ssel
+primary key (k1_katid, k2_katid) -- Definition eines zusammengesetzten PrimÃ¤rschlÃ¼ssel1, 2);
 );
